512568be1b326a44d20859cad61601c0a8779784
raxracks <multisniperism@gmail.com>
2022-04-17 23:28:01 +1200

    add entity creation and schema validation

--- db.js
+++ db.js
@@ -1,100 +1,136 @@
 const { gunzipSync, gzipSync } = require("zlib");
 const { readFileSync, writeFileSync } = require("fs");
+const { inherits } = require("util");
 
 class DB {
-    constructor(path) {
-        this.path = path;
-        this.queue = [];
-        this.data = "";
+  constructor(path, options = {}) {
+    this.path = path;
+    this.queue = [];
+    this.data = "";
 
-        this.read = function() {
-            this.data = gunzipSync(readFileSync(this.path, {flag:'r'})).toString();
+    if (options?.schema)
+      this.schema = JSON.parse(
+        readFileSync(options.schema, { flag: "r", encoding: "utf-8" })
+      );
+
+    this.validate = options?.validate ? options.validate : false;
+
+    this.remove = function (key) {
+      const item = `${key}:${this.get(key)}\0`;
+      this.data = this.data.replace(item, "");
+    };
+
+    this.queue_set = function (key, value) {
+      this.queue.push([key, value]);
+    };
+
+    this.queue_commit = function () {
+      for (let i = 0; i < this.queue.length; i++) {
+        const arr = this.queue[i];
+        const key = arr[0];
+        const value = arr[1];
+        const item = `${key}:${value}\0`;
+
+        if (!this.data.includes(`${key}:`)) {
+          this.data += item;
+        } else {
+          const value = this.data.split(`${key}:`)[1].split("\0")[0];
+          this.data = this.data.split(`${key}:${value}`).join(item);
         }
+      }
 
-        this.finalize = function() {
-            console.log("finalizing...");
-        
-            writeFileSync(this.path, gzipSync(this.data));
+      this.queue = [];
+    };
 
-            console.log("done");
+    this.create = function (entity, values) {
+      let s_entity = this.schema.entities[entity];
+      let obj = new Object();
+
+      if (!s_entity) throw new Error(`Unknown entity "${entity}".`);
+
+      Object.keys(values).forEach((item) => {
+        if (!s_entity[item])
+          throw new SchemaError(
+            `"${item}" is not in the entity "${entity}".`
+          );
+      });
+
+      Object.keys(s_entity).forEach((item) => {
+        if (values[item]) {
+          if (this.validate && typeof values[item] !== s_entity[item].type)
+            throw new SchemaError(
+              `Type of "${item}" (${typeof values[
+                item
+              ]}) does not match schema, expected type "${
+                s_entity[item].type
+              }".`
+            );
+
+          obj[item] = values[item];
+        } else {
+          if (!s_entity[item]["value"])
+            throw new SchemaError(
+              `"${item}" was not specified a value and has no default value.`
+            );
+
+          obj[item] = s_entity[item]["value"];
         }
+      });
 
-        this.format = function() {
-            const deflated = gzipSync("");
-        
-            writeFileSync(this.path, deflated);
-        }
+      return obj;
+    };
 
-        this.search = function(word) {
-            let results = [];
+    this.set = function (key, value) {
+      const item = `${key}:${value}\0`;
 
-            let expr = new RegExp(`(${word})\\w+`, 'gi');
-            let match = this.data.match(expr);
-            if(match !== null) results.push(...this.data.match(expr))
+      if (!this.data.includes(`${key}:`)) {
+        this.data += item;
+      } else {
+        const value = this.data.split(`${key}:`)[1].split("\0")[0];
+        this.data = this.data.split(`${key}:${value}`).join(item);
+      }
+    };
 
-            expr = new RegExp(`\\w+(${word})`, 'gi');
-            match = this.data.match(expr);
-            if(match !== null) results.push(...this.data.match(expr))
+    this.jsonify = function () {
+      const items = this.data.split("\0");
+      const json = {};
 
-            expr = new RegExp(`\\w+(${word})\\w+`, 'gi');
-            match = this.data.match(expr);
-            if(match !== null) results.push(...this.data.match(expr))
+      for (let i = 0; i < items.length; i++) {
+        const pair = items[i].split(/:(.*)/s);
+        const key = pair[0];
+        const value = pair[1];
+        json[key] = value;
+      }
 
-            return results;
-        }
+      return json;
+    };
 
-        this.queue_set = function(key, value) {
-            this.queue.push([key, value]);
-        }
+    this.get = function (key) {
+      if (!this.data.includes(`${key}:`)) return undefined;
+      return this.data.split(`${key}:`)[1].split("\0")[0];
+    };
 
-        this.queue_done = function() {        
-            for(let i = 0; i < this.queue.length; i++) {
-                const arr = this.queue[i];
-                const key = arr[0];
-                const value = arr[1];
-                const item = `\0${key}:${value}`;
-        
-                if(!this.data.includes(`${key}:`)) {
-                    this.data += item;
-                } else {
-                    const value = this.data.split(`${key}:`)[1].split("\0")[0];
-                    this.data = this.data.split(`${key}:${value}`).join(item);
-                }
-            }
-        
-            this.queue = [];
-        }
+    this.read = function () {
+      this.data = gunzipSync(readFileSync(this.path, { flag: "r" })).toString();
+    };
 
-        this.set = function(key, value) {        
-            const item = `\0${key}:${value}`;
+    this.format = function () {
+      const deflated = gzipSync("\0");
 
-            if(!this.data.includes(`${key}:`)) {
-                this.data += item;
-            } else {
-                const value = this.data.split(`${key}:`)[1].split("\0")[0];
-                this.data = this.data.split(`${key}:${value}`).join(item);
-            }
-        }
+      writeFileSync(this.path, deflated);
+    };
 
-        this.jsonify = function() {        
-            const items = this.data.split("\0");
-            const json = {};
-        
-            for(let i = 0; i < items.length; i++) {
-                const pair = items[i].split(/:(.*)/s);
-                const key = pair[0];
-                const value = pair[1];
-                json[key] = value;
-            }
-            
-            return json;
-        }
-
-        this.get = function(key) {
-            if(!this.data.includes(`${key}:`)) return undefined;
-            return this.data.split(`${key}:`)[1].split("\0")[0];
-        }
-    }
+    this.commit = function () {
+      writeFileSync(this.path, gzipSync(this.data));
+    };
+  }
 }
 
 module.exports.DB = DB;
+
+class SchemaError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "SchemaError";
+  }
+}
--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 const { DB } = require("./db.js");
-const database = new DB("test.db");
+const database = new DB("test.db", { schema: "./schema.json", validate: true });
 
 database.read();
-
-console.log(`raw data length: ${database.data.length}`);
-console.log(`search result: ${database.search("hello5130")}`);
+console.log(database.create("package", {
+    name: "ccjr",
+    user: "cjr-org",
+    repo: "ccjr-pkg"
+}));
